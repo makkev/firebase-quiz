0ccb1476a84001b3d96cf77ce08242b395bb0e62
K Mak <kumarajiva08@gmail.com>
2020-11-03 23:15:07 +1100

    get high score and name from Firebase and show on High Scores page

--- src/pages/high-scores.component.jsx
+++ src/pages/high-scores.component.jsx
@@ -1,13 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useFirebase } from '../components/firebase/firebase-context';
 
-export class HighScores extends Component {
-  render() {
-    return (
-      <>
-        <h1>High Scores</h1>
-      </>
-    );
-  }
-}
+const HighScores = () => {
+  const firebase = useFirebase();
+  const [scores, setScores] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    firebase.scores().once('value', snapshot => {
+      const data = snapshot.val();
+      const sortedScores = formatScoreData(data);
+      setScores(sortedScores);
+      setLoading(false);
+    });
+  });
+
+  const formatScoreData = firebaseScores => {
+    const scores = [];
+
+    for (let key in firebaseScores) {
+      const val = firebaseScores[key];
+      val['key'] = key;
+      scores.push(val);
+    }
+    return scores
+      .sort((score1, score2) => score2.score - score1.score)
+      .slice(0, 10);
+  };
+
+  return (
+    <div>
+      {loading && <div id="loader"></div>}
+      {!loading && (
+        <>
+          <h1>High Scores!!!</h1>
+          <div id="highScoresList">
+            {scores.map(record => (
+              <li key={record.key} className="high-score">
+                {record.name} - {record.score}
+              </li>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default HighScores;
